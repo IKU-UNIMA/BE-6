352efd23687cdeef5149d9718e8d6fa40f6bccec
Adhicitta Masran <adhicittamasran@gmail.com>
2023-05-25 23:10:33 +0800

    fix dashboard response

--- src/api/response/dashboard.go
+++ src/api/response/dashboard.go
@@ -10,24 +10,24 @@ type (
 	}
 
 	DashboardDetailPerFakultas struct {
-		ID          int    `json:"id"`
-		Fakultas    string `json:"fakultas"`
-		JumlahProdi int    `json:"jumlah_prodi"`
-		Jumlah      int    `json:"jumlah"`
-		Persentase  string `json:"persentase"`
+		ID               int    `json:"id"`
+		Fakultas         string `json:"fakultas"`
+		JumlahProdi      int    `json:"jumlah_prodi"`
+		JumlahPencapaian int    `json:"jumlah_pencapaian"`
+		Persentase       string `json:"persentase"`
 	}
 
 	DashboardPerProdi struct {
 		Fakultas   string                    `json:"fakultas"`
 		Total      int                       `json:"total"`
-		TotalDosen int                       `json:"total_dosen"`
+		TotalProdi int                       `json:"total_prodi"`
 		Pencapaian string                    `json:"pencapaian"`
 		Detail     []DashboardDetailPerProdi `json:"detail"`
 	}
 
 	DashboardDetailPerProdi struct {
-		Prodi   string `json:"prodi"`
-		Jumlah  int    `json:"jumlah"`
-		Capaian int    `json:"capaian"`
+		Prodi           string `json:"prodi"`
+		JumlahKerjasama int    `json:"jumlah_kerjasama"`
+		Capaian         int    `json:"capaian"`
 	}
 )
