f30c8626c2ba7db0fb7340b3df97bb763447c4c7
Adhicitta Masran <adhicittamasran@gmail.com>
2023-05-25 16:33:01 +0800

    remove unique constraint from nama in prodi model

--- src/model/prodi.go
+++ src/model/prodi.go
@@ -4,7 +4,7 @@ type Prodi struct {
 	ID         int `gorm:"primaryKey"`
 	IdFakultas int
 	KodeProdi  int      `gorm:"unique"`
-	Nama       string   `gorm:"type:varchar(255);unique"`
+	Nama       string   `gorm:"type:varchar(255)"`
 	Jenjang    string   `gorm:"type:varchar(60)"`
 	Fakultas   Fakultas `gorm:"foreignKey:IdFakultas;constraint:OnDelete:CASCADE"`
 }
